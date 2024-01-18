5d969e36d6a94245660e562eb5f25b9c2c8983fa
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-18 15:28:47 -0600

    just starting, taking notes and organizing thoughts

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('sample.md', md) {
+    fs.writeFile('sample.md', md, (err) =>
+        err ? console.error(err) : console.log(md));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
