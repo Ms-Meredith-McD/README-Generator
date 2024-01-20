85519cf5b6c7a4883c744fa1d20f40a8c80adb64
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-20 08:38:22 -0600

    comments on index.js

--- index.js
+++ index.js
@@ -3,8 +3,55 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
+// TODO: Create an array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        message: 'Please enter the title of your project.',
+        name: 'title',
+    },
 
+    {
+        type: 'input',
+        message: 'Please enter a description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please enter installation instuctions.',
+        name: 'installationInstructions',
+    },
+    {
+        type: 'input',
+        message: 'Please enter usage information.',
+        name: 'usageInformation',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the contribution guidelines.',
+        name: 'contributionGuidelines',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the test instructions.',
+        name: 'testInstructions',
+    },
+    {
+        type: 'list',
+        message: 'Which license would you like to add?',
+        name: 'license',
+        choices: ['MIT License', 'Apache License 2.0', 'GNU GPL v3','Mozilla Public License 2.0','BSD 3-Clause License']
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub name.',
+        name: 'githubName',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address.',
+        name: 'email',
+    }
     
 ];
 
