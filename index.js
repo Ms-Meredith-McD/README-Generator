190d08aa0668716bb3ae8cf3f5c1cb0a91dbe772
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-20 08:33:58 -0600

    added .gitignore

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
+
+
+const questions = [
+
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile('sample.md', md) {
-    fs.writeFile('sample.md', md, (err) =>
-        err ? console.error(err) : console.log(md));
+function writeToFile() {
+    fs.writeFile('sample.md', parseInt(questions), (err) =>
+        err ? console.error(err) : console.log(questions));
 }
 
 // TODO: Create a function to initialize app
