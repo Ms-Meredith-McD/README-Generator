3f1f6eda748e40faaa80148e15a2682ba2ce023e
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-22 20:22:41 -0600

    first try at writing template literal for the markdown readme page

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 async function renderLicenseBadge(license) {
   let licenseBadge;
-  if (questions.license === 'MIT'){
-    url = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-  } else if (questions.license === 'Apache 2.0') {
+  if (data.license === 'MIT'){
+    licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (data.license === 'Apache 2.0') {
     licenseBadge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-  } else if (questions.license === 'Mozilla Public License 2.0') {
+  } else if (data.license === 'Mozilla Public License 2.0') {
     licenseBadge = '![License: GPL v3](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)'
   } else {
     licenseBadge = ''
@@ -17,11 +17,11 @@ async function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   let licenseLink;
-  if (questions.license === 'MIT'){
+  if (data.license === 'MIT'){
     licenseLink = 'https://opensource.org/licenses/MIT'
-  } else if (questions.license === 'Apache 2.0') {
+  } else if (data.license === 'Apache 2.0') {
     licenseLink = 'https://opensource.org/licenses/Apache-2.0'
-  } else if (questions.license === 'Mozilla Public License 2.0') {
+  } else if (data.license === 'Mozilla Public License 2.0') {
     licenseLink = 'https://opensource.org/licenses/MPL-2.0'
   } else {
     licenseLink = ''
@@ -40,8 +40,44 @@ ${licenseBadge}`
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+${licenseBadge}
 
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## License
+${data.license}
+Please follow the link to learn more about the license: 
+${licenseLink}
+
+## Contributors
+
+${data.contributors}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+For any questions you may email ${email} or visit ${github}.
 `;
 }
 
